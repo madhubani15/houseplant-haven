bb1150b6677fbe67244a951f0d6770e6183feb9f
Mandabi <119072379+madhubani15@users.noreply.github.com>
2025-11-16 09:39:02 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,4 +3,19 @@ import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import LandingPage from './components/LandingPage';
 import ProductListing from './components/ProductListing';
-import CartPage from './components
+import CartPage from './components/CartPage';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/products" element={<ProductListing />} />
+        <Route path="/cart" element={<CartPage />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
