c2fa6633170c31fe9fb472477098b7f5c825691d
Mandabi <119072379+madhubani15@users.noreply.github.com>
2025-11-16 09:18:40 +0530

    Create plants.js

---
+++ src/data/plants.js
@@ -0,0 +1,48 @@
+// Six unique houseplants, three categories minimum
+const plants = [
+{
+id: 'p1',
+name: 'Fiddle Leaf Fig',
+price: 25.0,
+thumbnail: '/images/fiddle.jpg',
+category: 'Tall'
+},
+{
+id: 'p2',
+name: 'Snake Plant',
+price: 18.0,
+thumbnail: '/images/snake.jpg',
+category: 'Low Maintenance'
+},
+{
+id: 'p3',
+name: 'Pothos',
+price: 12.0,
+thumbnail: '/images/pothos.jpg',
+category: 'Hanging'
+},
+{
+id: 'p4',
+name: 'Monstera',
+price: 30.0,
+thumbnail: '/images/monstera.jpg',
+category: 'Tall'
+},
+{
+id: 'p5',
+name: 'ZZ Plant',
+price: 20.0,
+thumbnail: '/images/zz.jpg',
+category: 'Low Maintenance'
+},
+{
+id: 'p6',
+name: 'String of Pearls',
+price: 15.0,
+thumbnail: '/images/pearls.jpg',
+category: 'Hanging'
+}
+];
+
+
+export default plants;
